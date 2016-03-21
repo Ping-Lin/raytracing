452fe022defa01461b194b73de4fd67588e56400
Ping-Lin <billy3962@hotmail.com>
2016-03-22 00:47:50 +0800

    add inline force

--- math-toolkit.h
+++ math-toolkit.h
@@ -6,9 +6,7 @@
 #include <assert.h>
 #include <xmmintrin.h>
 
-__attribute__((always_inline))
-
-static inline
+static inline __forceinline
 void normalize(double *v)
 {
     double d = sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
@@ -19,13 +17,13 @@ void normalize(double *v)
     v[2] /= d;
 }
 
-static inline
+static inline __forceinline
 double length(const double *v)
 {
     return sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
 }
 
-static inline
+static inline __forceinline
 void add_vector(const double *a, const double *b, double *out)
 {
     out[0] = a[0] + b[0];
@@ -33,7 +31,7 @@ void add_vector(const double *a, const double *b, double *out)
     out[2] = a[2] + b[2];
 }
 
-static inline
+static inline __forceinline
 void subtract_vector(const double *a, const double *b, double *out)
 {
     //for (int i = 0; i < 3; i++)
@@ -42,7 +40,7 @@ void subtract_vector(const double *a, const double *b, double *out)
     out[2] = a[2] - b[2];
 }
 
-static inline
+static inline __forceinline
 void multiply_vectors(const double *a, const double *b, double *out)
 {
 	out[0] = a[0] * b[0];
@@ -50,7 +48,7 @@ void multiply_vectors(const double *a, const double *b, double *out)
 	out[2] = a[2] * b[2];
 }
 
-static inline
+static inline __forceinline
 void multiply_vector(const double *a, double b, double *out)
 {
     out[0] = a[0] * b;
@@ -58,7 +56,7 @@ void multiply_vector(const double *a, double b, double *out)
     out[2] = a[2] * b;
 }
 
-static inline
+static inline __forceinline
 void cross_product(const double *v1, const double *v2, double *out)
 {
     out[0] = v1[1] * v2[2] - v1[2] * v2[1];
@@ -66,7 +64,7 @@ void cross_product(const double *v1, const double *v2, double *out)
     out[2] = v1[0] * v2[1] - v1[1] * v2[0];
 }
 
-static inline
+static inline __forceinline
 double dot_product(const double *v1, const double *v2)
 {
 	/*double dp = 0.0;
@@ -88,7 +86,7 @@ double dot_product(const double *v1, const double *v2)
 	return v1[0] * v2[0] + v1[1] * v2[1] + v1[2] * v2[2];
 }
 
-static inline
+static inline __forceinline
 void scalar_triple_product(const double *u, const double *v, const double *w,
                            double *out)
 {
@@ -96,7 +94,7 @@ void scalar_triple_product(const double *u, const double *v, const double *w,
     multiply_vectors(u, out, out);
 }
 
-static inline
+static inline __forceinline
 double scalar_triple(const double *u, const double *v, const double *w)
 {
     double tmp[3];
